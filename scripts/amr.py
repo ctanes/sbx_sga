70035a98aa1176012108d127a57dea605250c29e
ctanes <ctanes@gmail.com>
2025-08-20 16:07:54 -0400

    AMR report now concats all the information

--- scripts/amr.py
+++ scripts/amr.py
@@ -1,55 +1,26 @@
-import csv
+import pandas as pd
 from pathlib import Path
-import sys
+import os
 
-def parse_file(file):
-    sample = Path(file)
-    sample_name = sample.parts[-2]
-    with open(file, "r") as file_obj:
-        filelines = file_obj.readlines()
-    return sample_name, filelines
+def summarize_file(amr_file):
+    """Return a dataframe of SNPs annotated with the sample name."""
+    if os.path.getsize(amr_file) > 0:
+        sample = Path(amr_file).parent.name
+        df = pd.read_csv(amr_file, sep="\t")
+        df.insert(0, "Sample", sample)
+    else:
+        df = pd.DataFrame()
+    return df
 
-
-# Expects a list of files as the argument
-def parse_scope(files):
-    subclassList = {}
-
-    for file in files:
-        sample, filelines = parse_file(file)
-        for line in filelines[1:]:
-            line = line.rstrip()
-            line_list = line.split("\t")
-            scope = line_list[8]
-            gene = line_list[5]
-
-            if scope == "AMR":
-                subclass = line_list[11]
-                if subclass not in subclassList:
-                    subclassList[subclass] = {}
-                if sample not in subclassList[subclass]:
-                    subclassList[subclass][sample] = [gene]
-                else:
-                    subclassList[subclass][sample].append(gene)
-            else:
-                continue
-    return subclassList
-
-
-def write_long_report(subclassList, output):
-    with open(output, "w", newline="") as tsv_file:
-        writer = csv.writer(tsv_file, delimiter="\t")
-        writer.writerow(["Sample", "Gene", "Category"])
-
-        for category, samples in subclassList.items():
-            for sample, genes in samples.items():
-                for gene in genes:
-                    writer.writerow([sample, gene, category])
+def summarize_all(files, output):
+    frames = [summarize_file(f) for f in files]
+    if frames:
+        df = pd.concat(frames)
+    else:
+        df = pd.DataFrame()
+    df.to_csv(output, sep="\t", index=False)
     return output
 
+summarize_all(snakemake.input, snakemake.output[0])
 
 
-input_reports = snakemake.input
-output = snakemake.output[0]
-
-subclass_list = parse_scope(input_reports)
-write_long_report(subclass_list, output)
