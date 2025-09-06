b8eaab7100ed08954bda5f4c9c26d40b02e84a6a
ctanes <ctanes@gmail.com>
2025-09-05 21:48:38 -0400

    Fixed bakta report

--- scripts/bakta.py
+++ scripts/bakta.py
@@ -1,9 +1,7 @@
 import sys
-from bakta_f import parse_file, get_annotation_stats, write_to_report
+from bakta_f import write_to_report
 
 report = snakemake.input[0]
 output = snakemake.output[0]
 
-filelines = open(report, "r").readlines()
-bakta_dict = parse_file(filelines)
-write_to_report(output, report, bakta_dict)
+write_to_report(report, output)
--- scripts/bakta_f.py
+++ scripts/bakta_f.py
@@ -1,9 +1,10 @@
 import sys
 import os
+import csv
 
 def parse_file(filelines):
+    parsed_dict = {}
     if len(filelines) != 0:
-        parsed_dict = {}
         for line in filelines:
             line = line.rstrip().split(":")
             try:
@@ -12,32 +13,31 @@ def parse_file(filelines):
             except:
                 continue
             parsed_dict[key] = value
-        return parsed_dict
-    else:
-        return {
-            "CRISPR arrays": "NA",
-            "hypotheticals": "NA",
-            "tRNAs": "NA",
-            "tmRNAs": "NA",
-            "rRNAs": "NA",
-            "CDSs": "NA",
-            "N50": "NA",
-            "Length": "NA",
-            "GC": "NA",
-        }
+    return parsed_dict
 
 
 def test_parse():
     lines = ['Annotation:\n', 'test: 9\n', 'a: 10\n', '\n', 'd: 1343\n']
     assert parse_file(lines) == {"Annotation":"", "test":"9", "a":"10", "d":"1343"}
 
-def write_to_report(output, genome, parsed_dict):
-    sample = os.path.splitext(os.path.basename(genome))[0]
-    with open(output, "w") as op:
-        op.write(
-            f"{sample}\t{parsed_dict['Length']}\t{parsed_dict['CDSs']}\t{parsed_dict['N50']}\t{parsed_dict['rRNAs']}\t{parsed_dict['tRNAs']}\t{parsed_dict['tmRNAs']}\t{parsed_dict['CRISPR arrays']}\t{parsed_dict['hypotheticals']}\t{parsed_dict['GC']}\n"
-        )
-    # The file is closed after this function, and only the path is returned.
-    return output
+def filter_keys(parsed_dict):
+    return {key: value for key, value in parsed_dict.items() if value != ""}
+
+def test_filter():
+    test = {"Annotation":"", "test":"9", "a":"10", "d":"1343"}
+    assert filter_keys(test) == {"test":"9", "a":"10", "d":"1343"}
+
+
+def write_to_report(report_fp, output_fp):
+    with open(report_fp, 'r') as f_in:
+        lines = f_in.readlines()
+
+    parsed_dict = parse_file(lines)
+    filtered = filter_keys(parsed_dict)
+
+    with open(output_fp, "w") as op:
+        writer = csv.writer(op, delimiter='\t')
+        writer.writerow(filtered.keys())
+        writer.writerow(filtered.values())
 
 
